467a41e6f6d21f4778d567743c24462d502b85a4
gankaixuan <92356668+gankaixuan@users.noreply.github.com>
2021-10-14 14:01:49 +0800

    add a file

---
+++ 简单运算.py
@@ -0,0 +1,11 @@
+num1=int(input("输入第一个数"))
+num2=int(input("输入第二个数"))
+x=input("输入一个运算符号")
+if x=="+":print(num1,"+",num2,"=",num1+num2)
+elif x=="-":print(num1,"-",num2,"=",num1-num2)
+elif x=="*":print(num1,"*",num2,"=",num1*num2)
+elif x=="/":print(num1,"/",num2,"=",num1/num2)
+else: print("输入错误，请重新输入")
+
+
+
