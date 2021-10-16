45e94c21a5bb66a4b23d3a0a45968957cbf5fb37
gankaixuan <92356668+gankaixuan@users.noreply.github.com>
2021-10-16 16:47:32 +0800

    Add files via upload
    Learn how to use operation symbols

---
+++ Learn how to use operation symbols.py
@@ -0,0 +1,22 @@
+a = 21
+b = 10
+c = 0
+c = a + b
+print("1-c的值为：",c)
+c = a - b
+print("2-c的值为：",c)
+c = a*b
+print("3-c的值为：",c)
+c=a/b
+print("4-c的值为：",c)
+c=a%b
+print("5-c的值为：",c)
+#修改变量a、b、c
+a=2
+b=3
+c=a**b
+print("6-c的值为：",c)
+a=10
+b=5
+c=a/b
+print("7-c的值为：",c)
