c70c67c77330cfff98224a66153d8b0b9e39a906
gankaixuan <92356668+gankaixuan@users.noreply.github.com>
2021-11-03 13:23:04 +0800

    Add files via upload

---
+++ 4-13.py
@@ -0,0 +1,16 @@
+i=1
+while i<=5:
+    j=1
+    while j<=i:
+        j+=1
+        print(" * ",end=" ")
+    i+=1
+    print("\n")
+i=4
+while i>=1:
+    j=1
+    while j<=i:
+        print(" * ",end=" ")
+        j+=1
+    i-=1
+    print("\n")
